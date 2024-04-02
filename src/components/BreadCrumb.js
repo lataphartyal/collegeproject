5b5d5192c1b5fd350010e59232e3d28e6e8595b7
lataphartyal <97403338+lataphartyal@users.noreply.github.com>
2024-04-03 00:43:07 +0530

    Add files via upload

--- src/components/BreadCrumb.js
+++ src/components/BreadCrumb.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const BreadCrumb = (props) => {
     const {title}= props;
   return (
-    <div className='breadcrumb mb-0 py-4'>
+    <div className='breadcrumb mb-0 py-2'>
       <div className='container-xxl'>
         <div className='row'>
             <div className='col-12'>
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactStars from "react-rating-stars-component";
-import { Link } from "react-router-dom";
-
-const ProductCard = ()=> {
+import { Link, useLocation } from "react-router-dom";
+const ProductCard = (props)=> {
+  const{grid}= props;
+    let location =useLocation();
   return (
-    <div className="col-3">
+   <>
+    <div className={`${location.pathname =="/store" ? `gr-${grid}` :"col-3"}`}>
       <div className="product-card position-relative">
         <div className="wishlist-icon postion-absolute">
           <Link>
@@ -39,8 +41,47 @@ const ProductCard = ()=> {
                 </Link>
             </div>
         </div>
+        </div>
       </div>
-    </div>
+      <div className={`${location.pathname =="/store" ? `gr-${grid}` :"col-3"}`}>
+      <div className="product-card position-relative">
+        <div className="wishlist-icon postion-absolute">
+          <Link>
+          <img src="images/wish.svg" alt="wishlist"/>
+          </Link>
+        </div>
+        <div className="product-image">
+          <img src="images/watch.jpg" alt="product" />
+        </div>
+        <div className="product-details">
+          <h6 className="brand">Havels</h6>
+          <h5 className="product-title">Various templates for the website</h5>
+          <ReactStars 
+          count={5} 
+          size={24} 
+          value={4}
+          edit={false}
+          activeColor="#ffd700" 
+          />
+          <p className="price">$100</p>
+        </div>
+        <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+                <Link> 
+                <img src="images/prodcompare.svg" alt="compare" /> 
+                </Link>
+                <Link> 
+                <img src="images/view.svg" alt="view" /> 
+                </Link>
+                <Link> 
+                <img src="images/add-cart.svg" alt="addcart" /> 
+                </Link>
+            </div>
+        </div>
+        </div>
+      </div>
+   </>
+    
   );
 }
 
